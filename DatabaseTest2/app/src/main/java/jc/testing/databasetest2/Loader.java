047bbbf7d4ecd5e287cd729bc8779f5a4403c503
Jack Chatterton <chattj@gmail.com>
2017-06-07 17:30:03 -0700

    bring in rxjava and use it to load data

--- DatabaseTest2/app/src/main/java/jc/testing/databasetest2/Loader.java
+++ DatabaseTest2/app/src/main/java/jc/testing/databasetest2/Loader.java
@@ -1,57 +1,89 @@
 package jc.testing.databasetest2;
 
-import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
 import org.yaml.snakeyaml.Yaml;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
+import io.reactivex.Emitter;
+import io.reactivex.Flowable;
+import io.reactivex.functions.BiConsumer;
 import jc.testing.databasetest2.model.ImmutableLegislator;
 import jc.testing.databasetest2.model.Legislator;
 
 public class Loader {
 
-    private Context context;
+    private final Context context;
 
     public Loader(Context c) {
         context = c;
     }
 
-    public List<Legislator> load() {
-        Yaml yaml = new Yaml();
-        List<Map<String, Map<String, String>>> list = null;
-        try {
-            list = (List<Map<String, Map<String, String>>>) yaml.load(context.getAssets().open("legislators-current.yaml"));
-        } catch (Exception e) {
-            Log.e("_JC", e.getMessage());
-        }
-        if (null != list) {
-            List<Legislator> loaded = new ArrayList<>();
-            for (Map<String, Map<String, String>> map : list) {
-                String party = ((List<Map<String, String>>)map.get("terms")).get(0).get("party");
-                Legislator.Party partyValue = Legislator.Party.OTHER;
-                if ("Democrat".equals(party)) {
-                    partyValue = Legislator.Party.DEMOCRAT;
-                } else if ("Republican".equals(party)) {
-                    partyValue = Legislator.Party.REPUBLICAN;
-                }
-                Legislator legislator = ImmutableLegislator.builder()
-                        .name(map.get("name").get("official_full"))
-                        .religion(map.get("bio").get("religion") != null ? map.get("bio").get("religion") : "")
-                        .termCount(((List)map.get("terms")).size())
-                        .party(partyValue)
-                        .id(map.get("id").get("wikidata"))
-                        .build();
-                loaded.add(legislator);
-                System.out.println(legislator);
+    @SuppressWarnings("unchecked")
+    private class LoaderState {
+        int position = 0;
+
+        private List<Map<String, Map<String, String>>> list = null;
+
+        void load() {
+            Yaml yaml = new Yaml();
+            try {
+                list = (List<Map<String, Map<String, String>>>) yaml.load(context.getAssets().open("legislators-current.yaml"));
+            } catch (Exception e) {
+                Log.e("_JC", e.getMessage());
             }
-            return loaded;
         }
-        return null;
+
+        int getLegislatorCount() {
+            if (null != list) {
+                return list.size();
+            }
+            return -1;
+        }
+
+        Legislator getLegislatorAtPosition(int position) {
+            Map<String, Map<String, String>> map = list.get(position);
+            String party = ((List<Map<String, String>>)map.get("terms")).get(0).get("party");
+            Legislator.Party partyValue = Legislator.Party.OTHER;
+            if ("Democrat".equals(party)) {
+                partyValue = Legislator.Party.DEMOCRAT;
+            } else if ("Republican".equals(party)) {
+                partyValue = Legislator.Party.REPUBLICAN;
+            }
+            return ImmutableLegislator.builder()
+                    .name(map.get("name").get("official_full"))
+                    .religion(map.get("bio").get("religion") != null ? map.get("bio").get("religion") : "")
+                    .termCount(((List)map.get("terms")).size())
+                    .party(partyValue)
+                    .id(map.get("id").get("wikidata"))
+                    .build();
+        }
+
+    }
+
+    public Flowable<Legislator> loadData() {
+        return Flowable.generate(new Callable<LoaderState>() {
+            @Override
+            public LoaderState call() throws Exception {
+                LoaderState loader = new LoaderState();
+                loader.load();
+                return loader;
+            }
+        }, new BiConsumer<LoaderState, Emitter<Legislator>>() {
+            @Override
+            public void accept(LoaderState loader, Emitter<Legislator> emitter) throws Exception {
+                if (loader.position >= loader.getLegislatorCount()) {
+                    emitter.onComplete();
+                } else {
+                    emitter.onNext(loader.getLegislatorAtPosition(loader.position));
+                    loader.position++;
+                }
+            }
+        });
     }
 
 }
--- DatabaseTest2/app/src/main/java/jc/testing/databasetest2/MainActivity.java
+++ DatabaseTest2/app/src/main/java/jc/testing/databasetest2/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import jc.testing.databasetest2.model.Legislator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadButtonClicked(View view) {
         Loader l = new Loader(this);
-        l.load();
+        l.loadData()
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Legislator>() {
+                    @Override
+                    public void accept(@NonNull Legislator legislator) throws Exception {
+                        System.out.println(legislator);
+                    }
+                });
     }
-
 
 }
